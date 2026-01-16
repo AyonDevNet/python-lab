500c7b7369e06605df971b02e216f884d15acb14
AyonDevNet <rmcoder7@gmail.com>
2026-01-16 11:17:02 +0600

    add

---
+++ loops/for_loop.py
@@ -0,0 +1,55 @@
+#A for loop is a control flow statement that allows you to execute a block of code repeatedly for each item in a sequence 
+#(like a list, string, or range) a predetermined number of times. 
+#It is used when the number of iterations is known or fixed.
+
+
+
+language = 'Python'
+
+for lan in language:
+    print(lan, end= '')
+
+
+#print apple nAME 
+
+fruits = ['apple' , 'mango' , 'licchi']
+
+for fruit in fruits:
+    print(fruit)
+
+
+#write a number 1 to 5
+
+for i in range (1,6):
+    print(i)
+
+#write a number 1 to 3
+
+for count in range (1,4):
+    print(count)
+
+#Print numbers from 1 to N Problem: Ask the user for a number N. 
+# Print all numbers from 1 to N using a for loop
+
+
+N = int(input("Enter a number: "))
+
+for i2 in range (1, N+1,):
+    print(i2)
+
+
+# Sum of first N natural numbers Problem: Input: N = 5 Output: 1 + 2 + 3 + 4 + 5 = 15
+
+
+# Ask the user for a number N
+N = int(input("Enter a number: "))
+
+# Initialize sum variable
+total = 0
+
+# Loop from 1 to N and add each number to total
+for i in range(1, N + 1):
+    total += i  # total = total + i
+
+# Print the result
+print(f"Sum of first {N} natural numbers is: {total}")
--- loops/while.py
+++ loops/while.py
@@ -24,7 +24,59 @@ print('hello broo ' +name)
 
 #Count from 1 to 10
 
-count = 1
-while count <= 10:
- print(count)
-count+=1
+
+count = 0
+while count < 11:
+     print(count)
+     count += 1
+
+
+#write a number 0 to 3
+
+count1 = 0
+while count1 < 4:
+  print(count1)
+  count1 += 1
+
+
+
+
+#Print numbers from 1 to N Problem: Ask the user for a number N. Print all numbers from 1 to N using a while loop
+
+asknumber = int(input('Enter the number: '))
+count2 = 1
+
+while count2 <= asknumber:
+    print(count2)
+    count2 += 1
+
+
+
+# Sum of first N natural numbers Problem: Input: N = 5 Output: 1 + 2 + 3 + 4 + 5 = 15
+
+N = int(input("Enter a number N: "))
+
+sum = 0
+count3 = 1
+while count3 <= N:
+   sum += count3
+   count3 += 1 
+
+print(f"The sum of first {N} natural numbers is: {sum}")
+
+
+
+
+
+
+#Print all even numbers up to N Problem:  Input: N = 20,  Output: 2 4 6 8 10 12 14 16 18 20
+ # Ask the user for input
+N = int(input("Enter a number N: "))
+
+# Start from 2 (first even number)
+number = 2
+
+# Loop until number is less than or equal to N
+while number <= N:
+    print(number, end=" ")  # Print number on the same line
+    number += 2             # Move to the next even number
